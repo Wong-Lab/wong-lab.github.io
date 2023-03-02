b2f29aa8833ae938fd2be5670e9ebf056c63fc75
Jonathan Chen <jwhc@ucla.edu>
2023-03-01 21:50:31 -0800

    reset

--- pubs/util.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-import yaml from 'js-yaml'
-
-export const loadYAML = path => new Promise((res, rej) => {
-  fs.readFile(path, {encoding: 'utf-8'}, (err, data) => {
-    (err) ? rej(err) : res(yaml.load(data))
-  })
-})
