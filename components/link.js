ac7e758449f6adaa43cdb859cd0a97c47f1cbc69
Jonathan Chen <jwhc@ucla.edu>
2023-09-26 15:50:23 -0700

    encode uris in links

--- components/link.js
+++ components/link.js
@@ -1,8 +1,8 @@
 import NextLink from 'next/link'
 
-export default function Link({ className, ...props }) {
+export default function Link({ className, href, ...props }) {
     return (
-        <NextLink className={`underline underline-offset-2 hover:decoration-2 text-sky-900 visited:text-sky-900 dark:text-[#84c9f2] ${className}`} {...props} />
+        <NextLink href={encodeURI(href)} className={`underline underline-offset-2 hover:decoration-2 text-sky-900 visited:text-sky-900 dark:text-[#84c9f2] ${className}`} {...props} />
     )
 }
 
